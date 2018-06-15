bf42f65d35f24c0e914f057ffe9706a11fd16013
Bonnie <yilee@umich.edu>
2018-06-15 12:41:55 +0800

    fix format

--- part2.py
+++ part2.py
@@ -11,14 +11,15 @@ command = sys.argv[1]
 
 #  python3 part2.py customers
 
-if command == "customer":
+if command == "customers":
 
 	statement1 = "SELECT Id, ContactName "
-	statement1 += "FROM %s" % command ;
+	statement1 += "FROM customer;"
 
 
 
-	print("Customer Table")
+	print("Id    | Customer Name")
+
 
 
 	rows_in_customer_table = cur.execute(statement1)
@@ -27,37 +28,73 @@ if command == "customer":
 		print ("%s | %s"%(ID, CustomerName))
 
 #  python3 part2.py employees
-elif command == "employee":
+elif command == "employees":
 
 
 
 	statement2 = "SELECT Id, FirstName, LastName "
-	statement2 += "FROM %s" % command ;
+	statement2 += "FROM employee;"
 
 
 
-	print("Employee Table")
+	print("Id | Employee Name")
 
 
 	rows_in_employee_table = cur.execute(statement2)
 	for row in rows_in_employee_table:
 		(ID, FirstName, LastName) = row;
-		print (ID, FirstName, LastName)
+		print ("%s  | %s %s"% (ID, FirstName, LastName))
 
 
-elif command == "order":
-
-#  python3 part2.py orders cust=<customer id>
-	command2 = sys.argv[2]
-	if command2[:4] == "cust":
-		#todo
+elif command == "orders":
+	option2 = sys.argv[2]    #  python3 part2.py orders cust=<customer id>
 	
+	command2, option3 = option2.split('=')
 
-	
+	if command2 == "cust":
+			
+		statement3 = "SELECT OrderDate "
+		statement3 += "FROM [Order] "
+		statement3 += "WHERE CustomerId = '%s' " %  option3  ;
+
+		
+		print ("Order Dates")
+
+		orderdate_by_id = cur.execute(statement3)
+		for row in orderdate_by_id:
+			OrderDate = row[0];
+			print (OrderDate)
+
 
 #  python3 part2.py orders emp=<employee last name>
-	elif command2[:3] == "emp":
-		#todo
+
+	
+
+	if command2 == "emp":
+
+		empLastName = option3
+			
+		statement4 = "SELECT OrderDate "
+		statement4 += "FROM [Order] "
+		statement4 += "WHERE EmployeeId = (SELECT EmployeeId FROM Employee WHERE LastName = '%s')" % empLastName ;
+		
+		print ("Order Dates")
+
+		orderdate_by_id = cur.execute(statement4)
+		for row in orderdate_by_id:
+			OrderDate = row[0];
+			print (OrderDate)		
+
+
+
+
+
+
+
+
+
+
+
 
 
 
